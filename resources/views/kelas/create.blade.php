ee5bd51112e0db23c9af7986e6b018efc33a3d91
neonexxa <firdaus@cyberhax.club>
2017-04-14 07:04:08 +0800

    repair create class

--- resources/views/kelas/create.blade.php
+++ resources/views/kelas/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="row">
     <div class="col s12 m6 offset-m3 z-depth-5" style="padding: 30px">
-        {!! Form::open(['route' => 'class.create', 'files' => true]) !!}
+        {!! Form::open(['route' => 'class.store', 'files' => true]) !!}
             @include('kelas.kelas',['form_name' => 'New Kelas','button_data' => 'Submit Kelas'])
         {!! Form::close() !!}
     </div>
